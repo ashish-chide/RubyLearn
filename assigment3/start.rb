d78befe9cb56cfaa7b203834b3437da551332c7e
aniketkhara <shubham.daundkar@wgblindia.com>
2019-02-27 19:48:13 +0530

    changes made in start file display message

--- assigment3/start.rb
+++ assigment3/start.rb
@@ -4,7 +4,7 @@ require_relative 'customer'
 
 class Launch
  def start
-    puts "if you are a Shopkeeper press 1 or a user then 2"
+    puts "If you are a Vendor press 1 or a Customer then 2"
     user_input = gets.strip.to_i
     if (user_input == 1)
       vendor = Vendor.new
