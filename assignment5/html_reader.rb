96301ec416a8e847d611ae19b622f2a6bf210a81
aniketkhara <shubham.daundkar@wgblindia.com>
2019-03-02 20:05:26 +0530

    made syntax changes according to Robocop error

--- assignment5/html_reader.rb
+++ assignment5/html_reader.rb
@@ -1,54 +1,56 @@
 require 'csv'
-
+# html reader
 class HtmlReader
+  TD = %r{(\<td\>)(.*)(\<\/td\>)}.freeze
+  TH = %r{(\<th\>)(.*)(\<\/th\>)}.freeze
+  TAG = %r{(<strong>|<\/strong>|<small>|<\/small>)}.freeze
 
-  def initialize(filename , csvfile)
+  def initialize(filename, csvfile)
     @filename = filename
-    @csvfile= csvfile
+    @csvfile = csvfile
   end
 
   def read_header
-   f = File.open(@filename , "r")
-   header_arr = []
-   f.each_line do |x|
-      y = x.gsub(/","/, ",")
-      match_data = /(\<th\>)(.*)(\<\/th\>)/.match(y).to_a
-      header_arr  << match_data[2].to_s.split("</th><th>")
+    f = File.open(@filename, 'r')
+    header_arr = []
+    f.each_line do |x|
+      y = x.gsub(/","/, ',')
+      match_data = TH.match(y).to_a
+      header_arr << match_data[2].to_s.split('</th><th>')
     end
-   set_header(header_arr)
+    write_header(header_arr)
   end
 
-  def set_header(header_arr)
-    if File.zero?(@csvfile)
-      File.open(@csvfile,"w") {|f| f.write(to_line(header_arr)) }
-      read_row
-    else
-      read_row
+  def write_header(header_arr)
+    File.open(@csvfile, 'w') do |f|
+      f.write(to_line(header_arr)) if File.zero?(@csvfile)
     end
+    read_row
   end
 
   def read_row
-  f = File.open(@filename , "r")
-  row_arr = []
-  f.each_line do |x|
-      y = x.gsub(/(<strong>|<\/strong>|<small>|<\/small>)/,"").gsub(/","/, ",").gsub(/, /, " ")
-      match_data = /(\<td\>)(.*)(\<\/td\>)/.match(y).to_a
-      row_arr << match_data[2].to_s.split("</td><td>")
+    f = File.open(@filename, 'r')
+    row_arr = []
+    f.each_line do |x|
+      y = x.gsub(TAG, '').gsub(/","/, ',').gsub(/, /, ' ')
+      match_data = TD.match(y).to_a
+      row_arr << match_data[2].to_s.split('</td><td>')
     end
-  set_row(row_arr)
+    write_row(row_arr)
   end
 
-  def set_row(row_arr)
-    row_arr.each_with_index do |row,index|
-      next if index == 0
-      File.open(@csvfile,"a") {|f| f.write(to_line(row)) }
+  def write_row(row_arr)
+    row_arr.each_with_index do |row, index|
+      next if index.zero?
+
+      File.open(@csvfile, 'a') { |f| f.write(to_line(row)) }
     end
   end
 
   def to_line(line)
-    a = line.join(",") << "\n"
+    line.join(',') << "\n"
   end
 end
 
-html = HtmlReader.new("vehicle_reports.html", "table.csv")
+html = HtmlReader.new('vehicle_reports.html', 'table.csv')
 html.read_header
